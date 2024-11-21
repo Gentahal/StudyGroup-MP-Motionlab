8ea892414ebf176131d12b505b1f68058f44630f
Genta Halilintar <gentahalilintar36@gmail.com>
2024-11-21 12:15:48 +0700

    first commit about kotlin

---
+++ app/src/main/java/com/example/firstapp_motion/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.firstapp_motion
+
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main) //connect to ui xml
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val usernameEditText = findViewById<TextInputLayout>(R.id.til_username)
+        val passwordEditText = findViewById<TextInputLayout>(R.id.til_password)
+
+        val login = findViewById<Button>(R.id.btn_login)
+
+        login.setOnClickListener {
+            val username = usernameEditText.editText?.text.toString()
+            val password = passwordEditText.editText?.text.toString()
+
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Username atau Password harus di isi", Toast.LENGTH_SHORT)
+                    .show()
+                Log.e(TAG, "login gagal")
+            } else {
+                Toast.makeText(this, "Berhasil", Toast.LENGTH_SHORT).show()
+
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(
+                    intent
+                )
+
+            }
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/firstapp_motion/ProfileActivity.kt
@@ -0,0 +1,29 @@
+package com.example.firstapp_motion
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_profile)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val home = findViewById<Button>(R.id.edit_profile_button)
+
+        home.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
