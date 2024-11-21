710ec194034f3838fa2a7a00f0a21f501ace9645
Genta Halilintar <gentahalilintar36@gmail.com>
2024-11-22 00:40:25 +0700

    first change

--- app/src/main/java/com/example/firstapp_motion/ProfileActivity.kt
+++ app/src/main/java/com/example/firstapp_motion/ProfileActivity.kt
@@ -2,11 +2,12 @@ package com.example.firstapp_motion
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +20,32 @@ class ProfileActivity : AppCompatActivity() {
             insets
         }
 
-        val home = findViewById<Button>(R.id.edit_profile_button)
+        val btnBack = findViewById<ImageView>(R.id.back_button)
 
-        home.setOnClickListener {
+        btnBack.setOnClickListener {
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
+        }
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottomNavigationView.selectedItemId = R.id.navigation_profile
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    startActivity(Intent(this, HomeActivity::class.java))
+                    true
+                }
+                R.id.navigation_search -> {
+                    startActivity(Intent(this, SearchActivity::class.java))
+                    true
+                }
+                R.id.navigation_profile -> {
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
--- app/src/main/java/com/example/firstapp_motion/SearchActivity.kt
+++ app/src/main/java/com/example/firstapp_motion/SearchActivity.kt
@@ -2,18 +2,17 @@ package com.example.firstapp_motion
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class HomeActivity : AppCompatActivity() {
+class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_search)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -22,16 +21,16 @@ class HomeActivity : AppCompatActivity() {
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
-        bottomNavigationView.selectedItemId = R.id.navigation_home
+        bottomNavigationView.selectedItemId = R.id.navigation_search
 
         bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.navigation_home -> {
+                R.id.navigation_search -> {
 
                     true
                 }
-                R.id.navigation_search -> {
-                    startActivity(Intent(this, SearchActivity::class.java))
+                R.id.navigation_home -> {
+                    startActivity(Intent(this, HomeActivity::class.java))
                     true
                 }
                 R.id.navigation_profile -> {
